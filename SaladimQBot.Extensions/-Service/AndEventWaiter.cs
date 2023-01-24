fad21655e5342a58f235285b6a123169250a7823
Saplonily <Saplonily@outlook.com>
2023-01-24 14:03:03 +0800

    加入`And`和`Or`事件waiter

---
+++ SaladimQBot.Extensions/-Service/AndEventWaiter.cs
@@ -0,0 +1,11 @@
+﻿namespace SaladimQBot.Extensions;
+
+public class AndEventWaiter : EventWaiter
+{
+    public override EventWaiterChecker Checker { get; }
+
+    public AndEventWaiter(EventWaiter waiterA, EventWaiter waiterB)
+    {
+        Checker = e => waiterA.Checker(e) && waiterB.Checker(e);
+    }
+}
