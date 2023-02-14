a530cd636e582d89e9acdeb13b1fb421d9731a9d
Saplonily <Saplonily@outlook.com>
2023-02-14 22:41:08 +0800

    parse的.net std2.0支持

--- SaladimQBot.UniNode/GenericUniNode.parse.cs
+++ SaladimQBot.UniNode/GenericUniNode.parse.cs
@@ -91,7 +91,74 @@ public partial class GenericUniNode
             }
         }
 #else
-        throw new NotImplementedException(".net std2.0 GenericUniNode parsing is not impl.");
+        var trimedStr = str.Substring(1, strLength - 2);
+        int primaryKeySpan = trimedStr.IndexOf(PrimaryKeyMarkChar);
+        int firstSplitCharInd = trimedStr.IndexOf(ArgSplitChar);
+        int firstEqualCharInd = trimedStr.IndexOf(ArgEqualChar);
+        if (primaryKeySpan != -1)
+        {
+            //有冒号, 带有主参
+            var nameSpan = trimedStr.Substring(0, primaryKeySpan);
+            string mainKey;
+            if (firstSplitCharInd != -1)
+            {
+                mainKey = trimedStr.Substring(primaryKeySpan + 1, firstSplitCharInd);
+                var argsSpan = trimedStr.Substring(firstSplitCharInd + 1);
+                if (TryParseArgString(argsSpan, out var dic))
+                {
+                    GenericUniNode node = new(client, nameSpan, dic!, mainKey);
+                    outNode = node;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (firstEqualCharInd != -1) return false;
+                mainKey = trimedStr.Substring(primaryKeySpan + 1);
+                GenericUniNode node = new(client, nameSpan, new Dictionary<string, string>(), mainKey);
+                outNode = node;
+                return true;
+            }
+        }
+        else
+        {
+            //无冒号, 无主参, 尝试寻找有没有其他参数
+            if (firstEqualCharInd == -1 && firstSplitCharInd == -1)
+            {
+                //无'=', 无',', 假设这是一个自结束的UniNode
+                GenericUniNode node = new(client, trimedStr, new Dictionary<string, string>());
+                outNode = node;
+                return true;
+            }
+            else
+            {
+                if (firstSplitCharInd != strLength - 1 && firstSplitCharInd != 0)
+                {
+                    //可能含有 '=', ','之一, 那么扔到参数解析器里尝试解析一下
+                    var nameSpan = trimedStr.Substring(0, firstSplitCharInd);
+                    var argsSpan = trimedStr.Substring(firstSplitCharInd + 1);
+
+                    if (TryParseArgString(argsSpan, out var dic))
+                    {
+                        GenericUniNode node = new(client, nameSpan, dic!);
+                        outNode = node;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
 #endif
     }
 
@@ -109,6 +176,30 @@ public partial class GenericUniNode
                 var leftKey = p[..equalCharPos];
                 var rightValue = p[(equalCharPos + 1)..];
                 dic.Add(Deescape(leftKey.ToString()), Deescape(rightValue.ToString()));
+            }
+            else
+            {
+                outDic = null;
+                return false;
+            }
+        }
+        outDic = dic;
+        return true;
+    }
+#else
+    //接收类似 'qq=2748166392' 或 'img=http://some.url,type=image' 的字串
+    internal static bool TryParseArgString(string chars, out Dictionary<string, string>? outDic)
+    {
+        Dictionary<string, string> dic = new();
+        var allSpans = chars.Split(ArgSplitChar);
+        foreach (var p in allSpans)
+        {
+            int equalCharPos = p.IndexOf(ArgEqualChar);
+            if (equalCharPos is not -1 or 0 && equalCharPos != p.Length - 1)
+            {
+                var leftKey = p.Substring(0, equalCharPos);
+                var rightValue = p.Substring(equalCharPos + 1);
+                dic.Add(Deescape(leftKey), Deescape(rightValue));
             }
             else
             {
